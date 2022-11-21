3870e0e979147b97ad7b6bdf5057801cc585f36c
Josh Levine <levine.joshuaadam@gmail.com>
2022-11-21 12:59:58 -0500

    initial setup for AddCourse form

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -2,21 +2,81 @@ import { useState } from 'react'
 import Client from '../services/api'
 
 
-const AddCourse = () => {
+const AddCourse = ({getAllCourses}) => {
 const initialFormValues = {
   name: '',
   description: '',
-  creditHours: 0
+  creditHours: ''
 }
 
-const [addCourseFormValues, setAddCourseFormValues] = useState({})
+const [addCourseFormValues, setAddCourseFormValues] = useState(initialFormValues)
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+    try {
+      // await Client.post('/courses', {
+      //   addCourseFormValues
+      // })
+      console.log(`add course requested: ${addCourseFormValues.name}, ${addCourseFormValues.description}, ${addCourseFormValues.creditHours}`)
+      setAddCourseFormValues(initialFormValues)
+      getAllCourses()
+    } catch (error) {
+      throw error
+    }
+}
+
+const handleChange = (e) => {
+  if(e.target.name === "creditHours") {
+    console.log('hello')
+    
+    //this is not working yet
+    const result = e.target.value.replace(/\D/g,'')
+    setAddCourseFormValues({
+      ...addCourseFormValues,
+      [e.target.name]: result,
+    })
+  }
+  setAddCourseFormValues({
+    ...addCourseFormValues,
+    [e.target.name]: e.target.value,
+  })
+}
+
 
   return (
-    <div>
-      <div className="courses-container">
-        <div>Course List</div>
-        <div>Add Course</div>
-      </div>
+    <div className="add-course-container form">
+      <form className="add-course-container form" onSubmit={handleSubmit}>
+
+      <h3> Add Course</h3>
+      <input
+          className="add-course-input"
+          onChange={handleChange}
+          type="text"
+          value={addCourseFormValues.name}
+          placeholder="Course Name"
+          id="add-course-input-name"
+          name="name"
+          />
+      <textarea
+          className="add-course-input"
+          onChange={handleChange}
+          // type="textarea"
+          value={addCourseFormValues.description}
+          placeholder="Course Description"
+          id="add-course-input-description"
+          name="description"
+          />
+      <input
+          className="add-course-input"
+          onChange={handleChange}
+          type="text"
+          value={addCourseFormValues.creditHours}
+          placeholder="Credit Hours"
+          id="add-course-input-creditHours"
+          name="creditHours"
+          />
+      <button className="add-course-form-btn btn">Submit</button>
+          </form>
     </div>
   )
 }
--- src/pages/AllCourses.js
+++ src/pages/AllCourses.js
@@ -1,13 +1,47 @@
 import AddCourse from '../components/AddCourse'
 import CourseList from '../components/CourseList'
+import { useState, useEffect } from 'react'
 
 const AllCourses = () => {
+  const [courses, setCourses] = useState([])
+  // let navigate = useNavigate()
+
+  const getAllCourses = async () => {
+    // let courseList = await Client.get('/courses/all')
+    // hardcoded - testing courselist
+    let courseList = [
+      {
+        PK: 1,
+        name: 'Biology 101',
+        description: 'its a biology class',
+        creditHours: 4
+      },
+      {
+        PK: 2,
+        name: 'English 101',
+        description: 'its an english class',
+        creditHours: 3
+      },
+      {
+        PK: 3,
+        name: 'CS 101',
+        description: 'its a CS class',
+        creditHours: 5
+      }
+    ]
+    setCourses(courseList)
+  }
+
+  useEffect(() => {
+    getAllCourses()
+  }, [])
+
   return (
     <div>
       <h1>All Courses</h1>
       <div className="course-container">
-        <CourseList />
-        <AddCourse />
+        <CourseList getAllCourses={getAllCourses} courses={courses} />
+        <AddCourse getAllCourses={getAllCourses} />
       </div>
     </div>
   )
